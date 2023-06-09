fdfe6cbf673fe0443fb7abed38d2f7736edf9505
quoc-hoang-cyper <73736235+quoc-hoang-cyper@users.noreply.github.com>
2023-06-10 00:53:16 +0700

    MVP Push

--- Virtual Fitting Room/mainroom.js
+++ Virtual Fitting Room/mainroom.js
@@ -19,10 +19,10 @@
 // }
 
 function changeProduct(evt, styleProduct) {
-    var i, product, productLinks;
-    product = document.getElementsByClassName("product");
-    for (i = 0; i < product.length; i++) {
-        product[i].style.display = "none";
+    var i, productStyle, productLinks;
+    productStyle = document.getElementsByClassName("productStyle");
+    for (i = 0; i < productStyle.length; i++) {
+        productStyle[i].style.display = "none";
     }
     productLinks = document.getElementsByClassName("productLinks");
     for (i = 0; i < productLinks.length; i++) {
@@ -59,6 +59,69 @@ function chooseBody(evt, model) {
     for (i = 0; i < bodyLinks.length; i++) {
         bodyLinks[i].className = bodyLinks[i].className.replace(" active", "");
     }
-    document.getElementById(model).style.display = "block";
+    document.getElementById(model).style.display = "inline-block";
+    evt.currentTarget.className += " active";
+}
+
+// shooseShirt
+
+function chooseShirt(evt, shirt) {
+    var i, shirtShow, shirtLinks;
+    shirtShow = document.getElementsByClassName("shirtShow");
+    for (i = 0; i < shirtShow.length; i++) {
+        shirtShow[i].style.display = "none";
+    }
+    shirtLinks = document.getElementsByClassName("shirtLinks");
+    for (i = 0; i < shirtLinks.length; i++) {
+        shirtLinks[i].className = shirtLinks[i].className.replace(" active", "");
+    }
+    document.getElementById(shirt).style.display = "inline-block";
+    evt.currentTarget.className += " active";
+}
+
+
+// choosePant
+
+function choosePant(evt, pant) {
+    var i, pantShow, pantLinks;
+    pantShow = document.getElementsByClassName("pantShow");
+    for (i = 0; i < pantShow.length; i++) {
+        pantShow[i].style.display = "none";
+    }
+    pantLinks = document.getElementsByClassName("pantLinks");
+    for (i = 0; i < pantLinks.length; i++) {
+        pantLinks[i].className = pantLinks[i].className.replace(" active", "");
+    }
+    document.getElementById(pant).style.display = "inline-block";
+    evt.currentTarget.className += " active";
+}
+
+// chooseStyle
+
+function chooseStyleShirt(evt, styleShirtFMS) {
+    var i, styleShirtTable, styleShirtLink;
+    styleShirtTable = document.getElementsByClassName("styleShirtTable");
+    for (i = 0; i < styleShirtTable.length; i++) {
+        styleShirtTable[i].style.display = "none";
+    }
+    styleShirtLink = document.getElementsByClassName("styleShirtLink");
+    for (i = 0; i < styleShirtLink.length; i++) {
+        styleShirtLink[i].className = styleShirtLink[i].className.replace(" active", "");
+    }
+    document.getElementById(styleShirtFMS).style.display = "inline-block";
+    evt.currentTarget.className += " active";
+}
+
+function chooseStylePant(evt, stylePantFMS) {
+    var i, stylePantTable, stylePantLink;
+    stylePantTable = document.getElementsByClassName("stylePantTable");
+    for (i = 0; i < stylePantTable.length; i++) {
+        stylePantTable[i].style.display = "none";
+    }
+    stylePantLink = document.getElementsByClassName("stylePantLink");
+    for (i = 0; i < stylePantLink.length; i++) {
+        stylePantLink[i].className = stylePantLink[i].className.replace(" active", "");
+    }
+    document.getElementById(stylePantFMS).style.display = "inline-block";
     evt.currentTarget.className += " active";
 }
